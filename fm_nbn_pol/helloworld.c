f22ce26dfc7d518ec73809036ff368493ebae7ab
Ahmad <ahmad.yusuf85@gmail.com>
2019-02-23 10:25:15 +1100

    1

--- fm_nbn_pol/helloworld.c
+++ fm_nbn_pol/helloworld.c
@@ -1,21 +1,27 @@
 #include "stdio.h"
 
-/*****************************************************
-* This the main function in this file
-* The main function will just call another  function called helloWorldFunction
-*****************************************************//**/
 void
-helloWorldFunction(char *input);
-int
-main()
+helloWorldFunction(
+	char *input);
+
+/*! \brief This the main function in this file.
+*
+* The main function will just call another  function called helloWorldFunction
+*******************************************************************************/
+int main()
 {
  helloWorldFunction("Hello World");
  return 0;
 }
 
 
+/*! @Brief This function will print Hello World to the screen
+*
+* The function will take a charcter as an input and will print the character
+* followed by a new line to the terminal
+*******************************************************************************/
 void
-helloWorldFunction(char *input)
+helloWorldFunction(char *input /*!< [in] */)
 {
  printf("%s\n",input);
 }
